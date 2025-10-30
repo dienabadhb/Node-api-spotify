194cfa88a125c77bd41184332ce9c1979b0f01c0
vanellazerr <v_effafegue@etu-webschoolfactory.fr>
2025-10-30 16:09:08 +0100

    add comment

--- src/models/song.ts
+++ src/models/song.ts
@@ -10,7 +10,7 @@ export class Song extends Model<InferAttributes<Song>, InferCreationAttributes<S
   declare cover_url: string;
   declare duration: number;
   declare audio_url: number
-
+  // Init model
   static initModel(sequelize: Sequelize): typeof Song {
     Song.init(
       {
